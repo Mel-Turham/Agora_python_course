87e36780cffdcb8e346beb65c38536da123c03f4
Mel/ <tchoikuemel06@gmail.com>
2024-06-19 16:52:24 +0100

    added other method from list type

--- course/list.py
+++ course/list.py
@@ -1,4 +1,5 @@
 users = ['John', 'Marc', 'Marry', 'Junior', 'Doe', 'Sara']
+lists_numbers = [ 4, 7, 7, 8 , 8 ,1 ,2 ,3]
 get_first_element_at_the_list = users[0]
 get_last_element_at_the_list = users[-1]
 
@@ -15,6 +16,19 @@ users.remove('Doe')
 is_user_exist = 'Sara' in users
 
 
+
 get_position_user = users.index('Sara')
 
 count_element = users.count('Sara')
+
+extend_list = users + ['max', 'man']
+
+list_filter = sorted(lists_numbers)
+lists_filter_reversed = sorted(lists_numbers, reverse=True)
+only_reversed = list(reversed(lists_numbers))
+copy_list_from_original = users.copy()
+
+# clear the list
+
+users.clear()
+
