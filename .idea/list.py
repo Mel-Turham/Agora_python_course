49b5c7342d6132a114617ba9191a96760bb19092
Mel/ <tchoikuemel06@gmail.com>
2024-06-19 16:33:57 +0100

    list

---
+++ .idea/list.py
@@ -0,0 +1,18 @@
+users = ['John', 'Marc', 'Marry', 'Junior', 'Doe', 'Sara']
+get_first_element_at_the_list = users[0]
+get_last_element_at_the_list = users[-1]
+
+users[0] = 'William'
+sub_users = users[2: 5]
+users.append('January')
+users.insert(3, 'Monday')
+
+users.pop(4)
+
+del users[2]
+
+users.remove('Doe')
+is_user_exist = 'Sara' in users
+print(is_user_exist)
+
+print(users)
